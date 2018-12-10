d91285b17626aea48da21b2f8b113e8beb8135cd
Franvcg <franvcg@gmail.com>
2018-12-10 17:35:29 -0200

    Comentários adicionados na classe Sprite

--- client/src/main/scala/pairs/client/phaser/pixi/Sprite.scala
+++ client/src/main/scala/pairs/client/phaser/pixi/Sprite.scala
@@ -3,8 +3,9 @@ package pairs.client.phaser.pixi
 import scala.scalajs.js
 import js.annotation._
 
-@js.native
-@JSGlobal("PIXI.Sprite")
+@js.native  //Indica que Phaser é nativo de uma biblioteca JavaScript
+@JSGlobal("PIXI.Sprite")  //Quando criamos uma classe com o nome diferente do que ela possui em JavaScript
+//é passado uma String com argumento da anotação @JSGlobal, indicando qual o nome dessa classe em JS.
 class Sprite protected () extends js.Object {
   var x: Double = js.native
   var y: Double = js.native
